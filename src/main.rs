637332b78053aa3bccf756c3cd1c18b3916acee0
Sam Schlinkert <sschlinkert@gmail.com>
2023-12-07 11:44:28 -0500

    less hashtags

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ impl TryFrom<i32> for GreaterThanZero {
 
 // And here's the fancier way, using the proc_macro2 crate
 // Based on https://github.com/dtolnay/syn/blob/master/examples/heapsize/heapsize_derive/src/lib.rs
+extern crate proc_macro;
 use quote::quote;
 
 #[proc_macro_derive(MyDerive)]
@@ -24,8 +25,8 @@ pub fn my_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = proc_macro2::TokenStream::from(input);
 
     let expanded = quote! {
-    // The generated impl.
-        impl #impl_generics heapsize::HeapSize for #name #ty_generics #where_clause {
+        // The generated impl.
+        impl TryFrom<i32> for GreaterThanZero {
             type Error = &'static str;
 
             fn try_from(value: i32) -> Result<Self, Self::Error> {
@@ -41,13 +42,14 @@ pub fn my_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     proc_macro::TokenStream::from(expanded)
 }
 
-fn main() {
-    let some_number = 101;
-    let another_greater_than_zero: GreaterThanZero =
-        GreaterThanZero::try_from(some_number).unwrap();
-    println!("{:?}", another_greater_than_zero);
+// Apparently "error: can't use a procedural macro from the same crate that defines it"
+#[derive(Debug, MyDerive)]
+struct GreaterThanZero2(i32);
 
-    let some_neg_number = -83;
-    let foo: GreaterThanZero = GreaterThanZero::try_from(some_neg_number).unwrap();
+fn main() {
+    let foo: GreaterThanZero = GreaterThanZero::try_from(101).unwrap();
     println!("{:?}", foo);
+
+    let bar: GreaterThanZero = GreaterThanZero::try_from(-83).unwrap();
+    println!("{:?}", bar);
 }
